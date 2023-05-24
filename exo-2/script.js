42f03e4926cc9713f546cb6e95c234c09c41057c
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-05-24 14:04:52 +0200

    étape 2

--- exo-2/script.js
+++ exo-2/script.js
@@ -1,2 +1,22 @@
 // you can write js here
 console.log('exo-2');
+
+const myDate = new Date(); // new Date('May 26, 2023 18:00:00') ou new Date('May 29, 2023 08:00:00')
+console.log(myDate);
+
+const isNotWeekEnd = "C'est pas encore le week-end bogoss";
+
+const isWeekEnd = "C'EST LE WEEK-END";
+
+let today = myDate.getDay();
+let options = { weekday: 'long'};
+console.log(today)
+
+if (today > 0 && today < 6) {
+    
+    console.log("On est que", new Intl.DateTimeFormat('fr-FR', options).format(myDate), ":(", isNotWeekEnd);
+} else {
+    console.log("On est", new Intl.DateTimeFormat('fr-FR', options).format(myDate), "!!!", isWeekEnd);
+}
+
+
