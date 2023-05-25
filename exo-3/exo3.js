82ad2a277d5303e41e156f750f8d6f431d585b87
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-05-25 09:37:18 +0200

    :three: étape 3

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -2,7 +2,9 @@
 console.log('exo-3');
 
 var playerInput = prompt("Your choice ( rock, paper, scissors) ?");
+playerInput = playerInput.toLowerCase();
+console.log(playerInput)
 
 function getPlayerChoice(playerInput) {
-    
+    return playerInput
 }
