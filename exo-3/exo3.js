83122193019c89e40b5232db55de396a577e1818
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-05-25 11:57:57 +0200

    :five: étape 5

--- exo-3/exo3.js
+++ exo-3/exo3.js
@@ -15,3 +15,32 @@ function getPlayerChoice(playerInput) {
 }
 
 getPlayerChoice(playerInput);
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * (3 - 0) + 0 );
+    console.log(randomNumber);
+
+    let possibleChoices = ["rock", "paper", "scissors"];
+
+    let choice;
+
+    if (randomNumber == 0) {
+        choice = possibleChoices[0];
+        console.log(choice);
+        return choice;
+    }
+
+    if (randomNumber == 1) {
+        choice = possibleChoices[1];
+        console.log(choice);
+        return choice;
+    }
+
+    if (randomNumber == 2) {
+        choice = possibleChoices[2];
+        console.log(choice);
+        return choice;
+    }
+}
+
+getComputerChoice();
