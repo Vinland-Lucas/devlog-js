f09a6140b4c6255dc21d2d7bbd29d282042175d9
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-05-25 16:22:18 +0200

    :sunglasses: final commit

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,43 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+console.log(secretMessage);
+
+//Etape 1 
+
+let eraseLast = secretMessage.pop();
+console.log("Etape 1 :", secretMessage);
+console.log("Etape 1 :", eraseLast);
+
+//Etape 2
+
+let addWordsLast = secretMessage.push("to", "program");
+console.log("Etape 2 :", secretMessage);
+
+//Etape 3
+
+secretMessage[6] = "right";
+console.log("Etape 3 :", secretMessage); 
+
+//Etape 4
+
+let eraseFirst = secretMessage.shift();
+console.log("Etape 4 :", secretMessage);
+console.log("Etape 4 :", eraseFirst);
+
+//Etape 5
+
+let addWordFirst = secretMessage.unshift("Programming");
+console.log("Etape 5 :", secretMessage);
+
+//Etape 6
+
+let deleteSeveralWords = secretMessage.splice(5, 5, "know");
+console.log("Etape 6 :", secretMessage);
+console.log("Etape 6 :", deleteSeveralWords);
+
+// Etape 7
+
+console.log("Etape 7 :", secretMessage.join());
+console.log(secretMessage.join(" "));
+console.log(secretMessage.join("-"));
