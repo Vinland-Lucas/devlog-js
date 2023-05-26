4ce1c2bce44b5ca1f453daeb78b02997f1718087
Lucas HOANG <lucas.hoang@le-campus-numerique.fr>
2023-05-26 14:55:45 +0200

    :sunglasses: final commit

--- exo-5/exo5.js
+++ exo-5/exo5.js
@@ -1,3 +1,39 @@
 // you can write js here
 
 console.log('exo-5');
+
+// Etape 1
+
+let input = "T'as les cramptes ? Apagnan ! Quoicoubeh !";
+input = input.toLowerCase();
+console.log(input);
+
+//Etape 2
+
+const vowels = ["a", "e", "i", "o","u","y"];
+
+// Etape 3
+
+let resultArray = [];
+
+// Etape 4 5 6 7 8 9
+
+for (i = 0; i < input.length; i++) {
+//console.log(input[i]);
+console.log(vowels.indexOf(input[i]));
+}
+
+for (let char of input) {
+    for (i = 0; i < vowels.length; i++) {
+        if (char === vowels[i]) {
+            console.log(char);
+            resultArray.push(char);
+        }
+    }
+}
+
+console.log(resultArray.join(" ").toUpperCase());
+
+
+
+
